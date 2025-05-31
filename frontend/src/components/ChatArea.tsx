c56cc36e71c8e2687641e6ea5a9d045bab3bb1b1
Parth Dhengle <parthdhengle12@gmail.com>
2025-05-31 17:00:23 +0530

    ui fix

--- frontend/src/components/ChatArea.tsx
+++ frontend/src/components/ChatArea.tsx
@@ -31,7 +31,7 @@ export const ChatArea: React.FC<ChatAreaProps> = ({
     <div className="flex-1 flex flex-col bg-gray-900 min-h-screen">
       {/* Chat Header */}
       <div className="border-b border-gray-700 p-4 bg-gray-800">
-        <h1 className="text-xl font-semibold text-white truncate">
+        <h1 className="text-xl font-semibold text-white truncate pl-14 pr-4">
           {chat.title}
         </h1>
       </div>
