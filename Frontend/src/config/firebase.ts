364a45a5ce24c11a2f27eccbe8a1472153f22a17
Parth Dhengle <parthdhengle12@gmail.com>
2025-06-03 15:00:22 +0530

    some minor changes

--- Frontend/src/config/firebase.ts
+++ Frontend/src/config/firebase.ts
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   // These are placeholder values - user will need to replace with their Firebase config
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyDjkOj1IxA5VyuB_0quIVI0oFLuvSmJ5jk",
+  authDomain: "convo-6ec96.firebaseapp.com",
+  projectId: "convo-6ec96",
+  storageBucket: "convo-6ec96.firebasestorage.app",
+  messagingSenderId: "279333232061",
+  appId: "1:279333232061:web:511814ee0bef0376c706d4",
+  measurementId: "G-8KKNTCVJP7"
 };
 
 const app = initializeApp(firebaseConfig);
